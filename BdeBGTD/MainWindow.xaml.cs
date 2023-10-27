0ab234928a56d0b587a71eddec554575911de1d0
zbzxx <2246762@bdeb.qc.ca>
2023-10-27 08:45:41 -0400

    Ajout date

--- BdeBGTD/MainWindow.xaml.cs
+++ BdeBGTD/MainWindow.xaml.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -20,11 +22,25 @@ namespace BdeBGTD
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DateTime dt;
         public static RoutedCommand AProprosCmd = new RoutedCommand();
         public MainWindow()
         {
             InitializeComponent();
+            dt = DateTime.Now;
+            AfficherDate(dt);
+            
         }
+        private void AfficherDate(DateTime dt)
+        {
+            DateText.Text = dt.ToString("yyyy-MM-dd");
+        }
+        private void BoutonPlus(object sender, ExecutedRoutedEventArgs e)
+        {
+            dt = dt.AddDays(1);
+            AfficherDate(dt);
+        }
+
         private void APropos_Executed(object sender, ExecutedRoutedEventArgs e)
         {
             MessageBox.Show("BdeB GTD\nVersion 1.0\nAuteur : Nicolas Werbrouck");
